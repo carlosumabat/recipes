d4599391c2e3aa580b5381cfb4ffbbb4e0913db4
Carlo Sumabat <sumabat.johncarlo@gmail.com>
2026-02-22 04:31:52 +0800

    Update environment variables for local testing

--- env.sh
+++ env.sh
@@ -1,3 +1,11 @@
 export SPRING_DATASOURCE_URL=jdbc:postgresql://localhost:5433/recipes
 export SPRING_DATASOURCE_USERNAME=user
 export SPRING_DATASOURCE_PASSWORD=pw
+export SPRING_APPLICATION_JSON='{
+  "spring.jpa.show-sql":true,
+  "logging.level.org.hibernate.SQL":"debug",
+  "logging.level.org.hibernate.orm.jdbc.bind":"trace",
+  "spring.jpa.properties.hibernate.format_sql":true
+}'
+
+export SPRING_OUTPUT_ANSI_ENABLED=ALWAYS
